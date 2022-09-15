e61def1e097d5ac7d0d44105641b729123c28810
Raphael <raphaelcsantos12@gmail.com>
2022-09-15 11:57:05 -0300

    Seção 5

--- Front/ProEventos2-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos2-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-    public eventos: any;
+    public eventos: any = [];
+    public eventosFiltrados: any = [];
+
+    widthImg: number = 150;
+    marginImg: number = 2;
+    visibilityImg: boolean = false;
+    private _filtroLista: string = '';
+
+    public get filtroLista(): string {
+      return this._filtroLista;
+    }
+
+    public set filtroLista(value: string){
+      this._filtroLista = value;
+      this.eventosFiltrados = this._filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+    }
+
+    filtrarEventos(filtrarPor: string): any {
+      filtrarPor = filtrarPor.toLocaleLowerCase();
+      return this.eventos.filter(
+        (evento: { tema: string; local: string}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      )
+    }
+
 //   [{
 //     Tema: 'Angular 12',
 //     Local: 'Belo Horizonte'
@@ -26,9 +50,15 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  changeVisibility(){
+    this.visibilityImg = !this.visibilityImg
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+      this.eventosFiltrados = this.eventos;},
       error => console.log(error)
     );
   }
